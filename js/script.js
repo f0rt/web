6e7bce67445205a1597fda2d59c0eb586c984533
f0rt <f012rt@gmail.com>
2024-03-27 22:01:22 +0200

    initial commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var splideElements = document.querySelectorAll('.splide');
+    
+    splideElements.forEach(function(splideElement) {
+        new Splide(splideElement,{
+                        type   : 'loop',
+                        pagination: true,
+                        arrows : false,
+                        height     : '30vh',
+                        autoWidth  : false,
+                        autoHeight : true,
+                    } ).mount();
+                }
+    );
+});
