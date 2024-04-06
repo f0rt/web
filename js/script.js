0bf7a7bfa40e467f52543cff99513ed0ef4b0fc0
f0rt <f012rt@gmail.com>
2024-04-07 01:13:41 +0300

    Added header splide. Started rooms section

--- js/script.js
+++ js/script.js
@@ -9,9 +9,11 @@ document.addEventListener('DOMContentLoaded', function() {
                         height     : '30vh',
                         autoWidth  : false,
                         autoHeight : true,
+                        autoplay   : true,
                     } ).mount();
                 }
     );
+
     let map;
 
     async function initMap() {
@@ -184,3 +186,24 @@ document.addEventListener('DOMContentLoaded', function() {
 
     initMap();
 });
+
+
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    // tablinks = document.getElementsByClassName("tablinks");
+    // for (i = 0; i < tablinks.length; i++) {
+    //     tablinks[i].className = tablinks[i].className.replace(" active", "");
+    // }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    // evt.currentTarget.className += " active";
+}
